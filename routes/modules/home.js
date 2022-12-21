7f9f873267b25a59cc325c2afca57ebf75ded4ca
WilliamChen15 <turelight13@gmail.com>
2022-12-21 23:05:35 +0800

    feat: routes,input check,print error

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const router = express.Router()
 
-const user = require('../../models/user')
+const User = require('../../models/user')
 
 // 路由
 
+router.get('/', (_req, res) => {
+  res.render('index')
+})
 
+router.post('/', (req, res) => {
+  const { email, password } = req.body
+  User.findOne({ email })
+    .lean()
+    .then(data => {
+      if (data.password === password) {
+        return res.render('show', { data })
+      }
+      const wrong = 'Username 或 Password 錯誤'
+      return res.render('index', { wrong , email})
+    })
+})
 
 module.exports = router
