b1983f673149ff4af2b6f947bddd3beb406728c7
WilliamChen15 <turelight13@gmail.com>
2022-12-26 17:11:55 +0800

    feat: set express-session ; save login state in session

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+// 引入 express-session 套件
+const session = require('express-session')
 
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
@@ -19,6 +21,14 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+// 建立 session 的 middleware，並將需要帶入的屬性放進 options 物件中
+app.use(session({
+  secret: 'sndkfnofnosfpowekmprwjqlierjw',
+  // 代表在每次與使用者互動後，不會強制把 session 儲存，除非 session 有變動
+  resave: false,
+  saveUninitialized: false
+  
+}))
 
 app.use(routes)
 
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,8 +5,23 @@ const User = require('../../models/user')
 
 // 路由
 
-router.get('/', (_req, res) => {
-  res.render('index')
+router.get('/', (req, res) => {
+  const logIn = req.session.isLoggedIn
+  const _id = req.session._id
+  // console.log('登入狀態:', logIn)
+  // console.log('使用者id:', _id)
+
+  // 若已經登入，顯示登入後畫面
+  if (logIn) {
+    User.findOne({ _id })
+      .lean()
+      .then(data => {
+        // console.log('撈到的資料:', data)
+        return res.render('show', { data })
+      })
+  } else {
+    res.render('index')
+  }
 })
 
 router.post('/', (req, res) => {
@@ -15,10 +30,12 @@ router.post('/', (req, res) => {
     .lean()
     .then(data => {
       if (data.password === password) {
+        req.session.isLoggedIn = true
+        req.session._id = data._id
         return res.render('show', { data })
       }
       const wrong = 'Username 或 Password 錯誤'
-      return res.render('index', { wrong , email})
+      return res.render('index', { wrong, email })
     })
 })
 
